af971ded5d1653594574634016da98e11a4f35ce
lucas-bresson <contact.bresson@gmail.com>
2024-10-24 17:37:49 +0200

    implement iPhone 3d Model section

---
+++ src/components/ModelView.tsx
@@ -0,0 +1,70 @@
+import { PerspectiveCamera, View, OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
+import { Suspense } from "react";
+import Lights from "./Lights";
+import IPhone from "./IPhone";
+import Loader from "./Loader";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  size,
+  item,
+}: {
+  index: number;
+  groupRef: React.MutableRefObject<THREE.Group<THREE.Object3DEventMap>>;
+  gsapType: string;
+  controlRef: any;
+  setRotationState: React.Dispatch<React.SetStateAction<number>>;
+  size: string;
+  item: {
+    title: string;
+    color: string[];
+    img: string;
+  };
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""}`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => {
+          if (controlRef.current) {
+            setRotationState(controlRef.current.getAzimuthalAngle());
+          }
+        }}
+      />
+      <group
+        ref={groupRef}
+        name={`${index === 1 ? "small" : "large"}`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<Loader />}>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+      <Lights />
+    </View>
+  );
+};
+
+export default ModelView;
---
+++ src/utils/animations.ts
@@ -0,0 +1,19 @@
+import * as THREE from 'three'
+
+export const animateWithGsapTimeline = (timeline:  gsap.core.Timeline, rotationRef: React.MutableRefObject<THREE.Group<THREE.Object3DEventMap>>, rotationState: number, firstTarget: string, secondTarget: string, animationProps: {transform: string, duration: number}) => {
+    timeline.to(rotationRef.current.rotation, {
+        y: rotationState,
+        duration: 1,
+        ease: 'power2.inOut'
+    })
+
+    timeline.to(firstTarget, {
+        ...animationProps,
+        ease: 'power2.inOut'
+    }, '<')
+
+    timeline.to(secondTarget, {
+        ...animationProps,
+        ease: 'power2.inOut'
+    }, '<')
+}
