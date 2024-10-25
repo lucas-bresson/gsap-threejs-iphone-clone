2ee630bd9535d9adc76c448dff3e59e57c0a62b0
lucas-bresson <contact.bresson@gmail.com>
2024-10-25 13:21:24 +0200

    implement Features section

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -1,4 +1,19 @@
 import * as THREE from 'three';
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger);
+
+export const animateWithGsap = (target: string, animationProps?: any, scrollProps?: any) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: 'restart reverse restart reverse',
+      start: 'top 85%',
+      ...scrollProps,
+    }
+  })
+}
 
 export const animateWithGsapTimeline = (
   timeline: gsap.core.Timeline,
